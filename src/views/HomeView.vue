6fb8dc87eb6ca57f3b0dabb364988db70d946627
Justyna <justyna.field@gmail.com>
2025-02-12 11:52:18 +0100

    Styled movie grid

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,6 @@ import { onMounted, ref } from 'vue'
 
 let movies = ref([])
 
-
 const getMovies = async () => {
   const apiKey = import.meta.env.VITE_API_KEY
   const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&include_video=false&language=en-US&page=1&sort_by=popularity.desc`)
@@ -19,18 +18,33 @@ onMounted(() => {
 </script>
 
 <template>
-  <main>
-    <!-- Movies list -->
-    <div class="grid grid-cols-3">
+  <div class="container">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid lg:grid-cols-3 gap-6 md:w-full p-10">
       <div
         v-for="movie in movies"
         :key="movie.id"
+        class="border rounded p-4 flex"
       >
-        <h2 class="font-bold">{{ movie.title }}</h2>
-        <sub>{{ movie.release_date }}</sub>
-        <img :src="`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`" />
-        <p>Rating: {{ movie.vote_average }}</p>
+        <div class="w-1/2">
+          <img :src="`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`" />
+        </div>
+        <div class="pl-6 w-1/2">
+          <div >
+            <h2 class="title">{{ movie.title }}</h2>
+            <sub class="text-md">{{ movie.release_date }}</sub>
+          </div>
+          <p>Rating: {{ movie.vote_average }}</p>
+        </div>
       </div>
     </div>
-  </main>
+  </div>
+
 </template>
+
+<style scoped>
+.title {
+  font-size: 20px;
+  line-height: 1;
+  font-weight: bold;
+}
+</style>
